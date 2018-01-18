46e468fa947f6812167d6908542d9824390be223
Luis Pabón <luis@portworx.com>
2018-01-18 09:11:03 -0500

    Update container builder

--- containers/mock-plugin/make.sh
+++ containers/mock-plugin/make.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 docker build -f Dockerfile.builder -t mocktest:builder .
-docker run --privileged -v $PWD:/host mocktest:builder cp /go/bin/mock /host/mock
-docker run --privileged -v $PWD:/host mocktest:builder cp /go/bin/csc /host/csc
+docker run --rm --privileged -v $PWD:/host mocktest:builder cp /go/bin/mock /host/mock
+docker run --rm --privileged -v $PWD:/host mocktest:builder cp /go/bin/csc /host/csc
 sudo chown $USER mock
 sudo chown $USER csc
-docker build -t docker.io/k8scsi/mock-plugin .
+docker build -t quay.io/k8scsi/mock-plugin .
 rm -f mock csc
+docker rmi mocktest:builder
